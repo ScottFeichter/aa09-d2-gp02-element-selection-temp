f377b1fc989b1321b15f7da0593070974c5f5364
Scott Feichter <scottfeichter@gmail.com>
2024-04-16 10:06:27 -0700

    try to refactor last one

--- selectors.js
+++ selectors.js
@@ -1,38 +1,74 @@
 const select = () => {
-    /* Write queries for each of the following */
+  /* Write queries for each of the following */
 
-    /* Section 1 */
-    // 1. Get all seeded fruit elements
-    // Your code here 
-    
-    // 2. Get all seedless fruit elements
-    // Your code here 
+  /* Section 1 */
+  // 1. Get all seeded fruit elements
+  // Your code here
+  const seedFruitElements = document.getElementsByClassName("seed");
+  const seedFruitNodes = document.querySelectorAll(".seed");
+  console.log(seedFruitElements);
+  console.log(seedFruitNodes);
 
-    // 3. Get first seedless fruit element
-    // Your code here 
+  // 2. Get all seedless fruit elements
+  // Your code here
+  const seedlessFruitElements = document.getElementsByClassName("seedless");
+  const seedlessFruitNodes = document.querySelectorAll(".seedless");
 
-    /* Section 2 */
-    // 4. Get inner span with text "you"
-    // Your code here 
+  console.log(seedlessFruitElements);
+  console.log(seedlessFruitNodes);
 
-    // 5. Get all children of element "wrapper"
-    // Your code here 
+  // 3. Get first seedless fruit element
+  // Your code here
+  const firstSeedless = document.querySelector(`.seedless`);
+  console.log(firstSeedless);
 
-    // 6. Get all odd number list items in the list
-    // Your code here 
+  /* Section 2 */
+  // 4. Get inner span with text "you"
+  // Your code here
+  const youSpan = document.querySelector("#wrapper span");
+  console.log(youSpan);
 
-    // 7. Get all even number list items in the list
-    // Your code here 
+  // 5. Get all children of element "wrapper"
+  // Your code here
+  const wrapperChildren = document.getElementById("wrapper").children;
+  console.log(wrapperChildren);
 
-    /* Section 3 */
-    // 8. Get all tech companies without a class name
-    // Your code here 
+  // 6. Get all odd number list items in the list
+  // Your code here
+  const oddsWeirdGrabTWSS = document.querySelectorAll(
+    `#two > ol > li:nth-child(even)`
+  );
+  console.log(oddsWeirdGrabTWSS);
+  const oddsHTMLCollection = document.body.getElementsByClassName("odd");
+  console.log(oddsHTMLCollection);
+  const oddsNodeList = document.querySelectorAll(`#two > ol > .odd`);
+  console.log(oddsNodeList);
 
-    // 9. Get "Amazon" list element
-    // Your code here 
+  // 7. Get all even number list items in the list
+  // Your code here
+  const evensNodeList = document.querySelectorAll(`#two > ol > li:not(.odd)`);
+  console.log(evensNodeList);
 
-    // 10. Get all unicorn list elements (not the image element)
-    // Your code here 
-}
+  /* Section 3 */
+  // 8. Get all tech companies without a class name
+  // Your code here
+  const companiesWithNoClass = document.querySelectorAll(
+    `#three a:not([class])`
+  );
+  console.log(companiesWithNoClass);
+
+  // 9. Get "Amazon" list element
+  // Your code here
+  const amazon = document.querySelectorAll(
+    `#three a[href="https://www.amazon.com"]`
+  );
+  console.log(amazon);
+
+  // 10. Get all unicorn list elements (not the image element)
+  // Your code here
+  const listElements = document.querySelectorAll(`.unicorn`);
+  console.log(listElements);
+  // wrong thing - try to refactor
+};
 
 window.onload = select;
